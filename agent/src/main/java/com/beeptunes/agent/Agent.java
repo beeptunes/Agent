1298c14639076d453a2086591c92e06a15315e80
Javad Arjmandi <javadarjmandi@gmail.com>
2019-02-05 05:42:51 +0330

    added copyright notice

--- agent/src/main/java/com/beeptunes/agent/Agent.java
+++ agent/src/main/java/com/beeptunes/agent/Agent.java
@@ -21,6 +21,23 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 /*
+Copyright 2019 https://beeptunes.ca/
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
+
+/*
     Only 2 public methods. One for initializing The Agent in Application class,
     second one for using the agent's methods
 */
@@ -117,7 +134,7 @@ public class Agent {
     }
 
     public void searchForArtists(String key, Integer size, Integer page, AgentCallback<SearchResult.Artists> callback){
-        request(client.getAgent().searchAlbums(key, size, page), callback);
+        request(client.getAgent().searchArtists(key, size, page), callback);
     }
 
     public void start(ListenInfo info, AgentCallback<ResponseBody> callback){
