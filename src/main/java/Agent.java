4f78b04b5c59443eceff4ec7589cebef685cb5ce
Javad Arjmandi <javadarjmandi@gmail.com>
2019-02-10 04:32:59 +0330

    added exception for when Agent is not initialized

--- src/main/java/Agent.java
+++ src/main/java/Agent.java
@@ -1,24 +1,26 @@
+/*
+ * Copyright 2019 https://beeptunes.ca
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 import Models.*;
 import java.util.List;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
-/*
-Copyright 2019 https://beeptunes.ca/
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
 
 
 /**
@@ -28,15 +30,14 @@ Copyright 2019 https://beeptunes.ca/
 
 public class Agent {
 
-    private static AgentClient client;
     private static String apiKey = "";
+    private static AgentClient client = new AgentClient(apiKey);
     //--------------------------------
     private static Agent instance = null;
 
     private Agent(String apiKey){
-        client = new AgentClient(apiKey);
+        client.setApiKey(apiKey);
     }
-
 
     public static void init(String apiKey){
         if(instance == null)
@@ -51,72 +52,69 @@ public class Agent {
             return instance;
     }
 
-    public static Agent get(){
-        return getInstance();
-    }
-
-    public static void search(String s , AgentCallback<SearchResult> callback){
-        getInstance().searchAll(s, callback);
-    }
-
-
-    public void latestAlbums (Integer size, Integer page, AgentCallback<SearchResult.Albums> callback) {
+    public static void latestAlbums (Integer size, Integer page, AgentCallback<SearchResult.Albums> callback) {
         request(client.getAgent().getLatestAlbums(size, page), callback);
     }
 
-    public void album (Long id, AgentCallback<Album> callback) {
+    public static void album (Long id, AgentCallback<Album> callback) {
         request(client.getAgent().getAlbum(id), callback);
     }
 
-    public void track (Long id, AgentCallback<Track> callback) {
+    public static void track (Long id, AgentCallback<Track> callback) {
         request(client.getAgent().getTrack(id), callback);
     }
 
-    public void downloadLinks (Long id, AgentCallback<DownloadLinks> callback) {
+    public static void downloadLinks (Long id, AgentCallback<DownloadLinks> callback) {
         request(client.getAgent().getDownloadLinks(id), callback);
     }
 
-    public void artist (Long id, AgentCallback<Artist> callback) {
+    public static void artist (Long id, AgentCallback<Artist> callback) {
         request(client.getAgent().getArtist(id), callback);
     }
 
-    public void albumTrackList (Long id, AgentCallback<List<Track>> callback) {
+    public static void albumTrackList (Long id, AgentCallback<List<Track>> callback) {
         request(client.getAgent().getAlbumTracks(id), callback);
     }
 
-    public void artistAlbums (Long id, Integer page, Integer size, AgentCallback<SearchResult.Albums> callback) {
+    public static void artistAlbums (Long id, Integer page, Integer size, AgentCallback<SearchResult.Albums> callback) {
         request(client.getAgent().getArtistAlbums(id, page, size), callback);
     }
 
-    private void searchAll(String term, AgentCallback<SearchResult> callback){
+    private static void searchAll(String term, AgentCallback<SearchResult> callback){
         request(client.getAgent().search(term), callback);
     }
 
-    public void searchForAlbums(String key, Integer page, Integer size, AgentCallback<SearchResult.Albums> callback){
+    public static void searchForAlbums(String key, Integer page, Integer size, AgentCallback<SearchResult.Albums> callback){
         request(client.getAgent().searchAlbums(key, size, page), callback);
     }
 
-    public void searchForTracks(String key, Integer size, Integer page, AgentCallback<SearchResult.Tracks> callback){
+    public static void searchForTracks(String key, Integer size, Integer page, AgentCallback<SearchResult.Tracks> callback){
         request(client.getAgent().searchTracks(key, size, page), callback);
     }
 
-    public void searchForArtists(String key, Integer size, Integer page, AgentCallback<SearchResult.Artists> callback){
+    public static void searchForArtists(String key, Integer size, Integer page, AgentCallback<SearchResult.Artists> callback){
         request(client.getAgent().searchArtists(key, size, page), callback);
     }
 
-    public void start(ListenInfo info, AgentCallback<ResponseBody> callback){
+    public static void start(ListenInfo info, AgentCallback<ResponseBody> callback){
         request(client.getAgent().start(info), callback);
     }
 
-    public void finish(ListenInfo info, AgentCallback<ResponseBody> callback){
+    public static void finish(ListenInfo info, AgentCallback<ResponseBody> callback){
         request(client.getAgent().finish(info), callback);
     }
 
-    private void request(final Call call, final AgentCallback callback){
+    private static void request(final Call call, final AgentCallback callback){
         call.enqueue(new Callback() {
-
             @Override
             public void onResponse (Call call, Response response) {
+
+                if(response.code() == 401) try { // Token is invalid or client not initialized
+                    throw new ApiKeyNotFoundException();
+                } catch (ApiKeyNotFoundException e) {
+                    e.printStackTrace();
+                }
+
                 callback.onResponse(call, response);
             }
 
--- src/main/java/AgentCallback.java
+++ src/main/java/AgentCallback.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2019 https://beeptunes.ca
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -7,27 +24,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-/*
-Copyright 2019 https://beeptunes.ca/
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- */
 
 public abstract class AgentCallback<T> implements Callback<T> {
 
     protected int statusCode = 0;
     Gson gson = new GsonBuilder().create();
-    protected Error error;
+    protected Models.Error error;
 
     private final String TAG = this.getClass().getSimpleName();
 
@@ -44,7 +46,7 @@ public abstract class AgentCallback<T> implements Callback<T> {
         if(response.code() >= 400) { //Error
             try {
                 assert response.errorBody() != null;
-                error = gson.fromJson(response.errorBody().string(), Error.class);
+                error = gson.fromJson(response.errorBody().string(), Models.Error.class);
                 onFailure(call, new Throwable(response.errorBody().string()));
             } catch (IOException e) {
                 e.printStackTrace();
